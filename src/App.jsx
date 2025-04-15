0e1ba2ec10631d145cd250a42c91b9318d5821d3
Harsh Raj <samarshelley@gmail.com>
2025-04-15 10:26:53 +0530

    files and folder initialised

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Navbar from './component/Navbar'
+import Hero from './component/Hero'
+import Services from './component/Services'
+import About from './component/About'
+import Tours from './component/Tours'
+import Footer from './component/Footer'
+
+const  App = () => {
+  return (
+    <div>
+      <h1>HARSH</h1>
+      <Navbar/>
+      <Hero/>
+      <Services/>
+      <Tours/>
+      <About/>
+      <Footer/>
+      
+    </div>
+  )
+}
+
+export default App
