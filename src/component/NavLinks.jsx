f56ae2a10b416dd4cfcd427f5f093a6fcee3beb1
Harsh Raj <samarshelley@gmail.com>
2025-04-16 12:27:30 +0530

    navbar improved

---
+++ src/component/NavLinks.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const  Menu=({name , link})=> {
+
+    return (
+        <li>
+        <a href="{link}" className="nav-link"> {name} </a>
+      </li>
+    )
+}
+
+const NavLinks = () => {
+  return (
+    <ul className="nav-links" id="nav-links">
+   {menuItems.map((item)=>{
+    return <Menu {...item} />
+   })}
+  </ul>
+  )
+}
+
+export default NavLinks
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -12,24 +12,9 @@ function Navbar() {
             <i className="fas fa-bars"></i>
           </button>
         </div>
+
         {/* <!-- left this comment on purpose --> */}
-        <ul className="nav-links" id="nav-links">
-          <li>
-            <a href="#home" className="nav-link"> home </a>
-          </li>
-
-          <li>
-            <a href="#about" className="nav-link"> about </a>
-          </li>
-
-          <li>
-            <a href="#services" className="nav-link"> services </a>
-          </li>
-
-          <li>
-            <a href="#tours" className="nav-link"> tours</a>
-          </li>
-        </ul>
+      
 
         <ul className="nav-icons">
           <li>
